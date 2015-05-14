6a7b12b42958dbced6284893bbde1ca35930a4fa
Malgorzata Lewandowska <ledm@viessmann.com>
2015-05-14 12:03:34 +0200

    Initial Commit

---
+++ MilaStripeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MilaStripeView
+//
+//  Created by malgorzata on 14/05/15.
+//  Copyright (c) 2015 MilaMeko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MilaStripeView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MilaStripeView
+//
+//  Created by malgorzata on 14/05/15.
+//  Copyright (c) 2015 MilaMeko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
